e0a2e5eda53178ce4f6e567a6b8f66f293980467
skye-fivecents <duncan.fivecents@gmail.com>
2026-02-05 11:01:35 -0700

    pong!!! i think we do collision next class

---
+++ Lesson07_Pong/Lesson07_Pong.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Lesson07_Pong;
+
+public class Pong : Game
+{
+    private const int _WindowWidth = 750, _WindowHeight = 450, _BallWidthAndHeight = 21;
+    private const int _PlayAreaEdgeLineWidth = 12;
+    private const int _PaddleWidth = 8, _PaddleHeight = 124;
+    private const float _PaddleSpeed = 240, _BallSpeed = 100;
+
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+
+    private Texture2D _backgroundTexture, _ballTexture, _paddleTexture;
+
+    private Vector2 _ballPosition, _ballDirection;
+    private float _ballSpeed;
+
+    private Vector2 _paddlePosition, _paddleDirection, _paddleDimensions;
+    private Vector2 _paddle2Position, _paddle2Direction, _paddle2Dimensions;
+    private float _paddleSpeed, _paddle2Speed;
+
+
+    internal Rectangle PlayAreaBoundingBox
+    {
+        get
+        {
+            return new Rectangle(0, _PlayAreaEdgeLineWidth, _WindowWidth, _WindowHeight - 2*_PlayAreaEdgeLineWidth);
+        }
+    }
+
+    public Pong()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        _graphics.PreferredBackBufferWidth = _WindowWidth;
+        _graphics.PreferredBackBufferHeight = _WindowHeight;
+        _graphics.ApplyChanges();
+
+        _ballPosition = new Vector2(150, 195);
+        _ballSpeed = _BallSpeed;
+        _ballDirection.X = -1;
+        _ballDirection.Y = -1;
+
+        _paddlePosition = new Vector2(690, 198);
+        _paddleSpeed = _PaddleSpeed;
+        _paddleDimensions = new Vector2(_PaddleWidth,_PaddleHeight);
+        _paddleDirection = Vector2.Zero;
+
+        _paddle2Position = new Vector2(52, 198);
+        _paddle2Speed = _PaddleSpeed;
+        _paddle2Dimensions = new Vector2(_PaddleWidth,_PaddleHeight);
+        _paddle2Direction = Vector2.Zero;
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        _backgroundTexture = Content.Load<Texture2D>("Court");
+        _ballTexture = Content.Load<Texture2D>("Ball");
+        _paddleTexture = Content.Load<Texture2D>("Paddle");
+    }
+
+        protected override void Update(GameTime gameTime)
+    {
+        float dt = (float) gameTime.ElapsedGameTime.TotalSeconds;
+        KeyboardState kbState = Keyboard.GetState();
+
+        #region Ball Movement
+            _ballPosition += _ballDirection * _ballSpeed * dt;
+
+            //bounce the ball off left and right sides
+            if(_ballPosition.X <= PlayAreaBoundingBox.Left || 
+                _ballPosition.X + _BallWidthAndHeight >= PlayAreaBoundingBox.Right)
+            {
+                _ballDirection.X *= -1;
+            }
+            //in-class exercise: make the ball bounce off of the top and bottom of the play area bounding box
+            if(_ballPosition.Y <= PlayAreaBoundingBox.Top || 
+                _ballPosition.Y + _BallWidthAndHeight >= PlayAreaBoundingBox.Bottom)
+            {
+                _ballDirection.Y *= -1;
+            }
+        #endregion
+
+        #region Right Player Movement
+            if(kbState.IsKeyDown(Keys.Up))
+            {
+                _paddleDirection = new Vector2(0,-1);
+            }
+            else if(kbState.IsKeyDown(Keys.Down))
+            {
+                _paddleDirection = new Vector2(0,1);
+            }
+            else
+            {
+                _paddleDirection = Vector2.Zero;
+            }
+
+            _paddlePosition += _paddleDirection * _paddleSpeed * dt;
+
+            if(_paddlePosition.Y <= PlayAreaBoundingBox.Top)
+            {
+                _paddlePosition.Y = PlayAreaBoundingBox.Top;   
+            }
+            else if(_paddlePosition.Y + _paddleDimensions.Y >= PlayAreaBoundingBox.Bottom)
+            {
+                _paddlePosition.Y = PlayAreaBoundingBox.Bottom - _paddleDimensions.Y;
+            }
+        #endregion
+
+        #region Left Player Movement
+            if(kbState.IsKeyDown(Keys.W))
+            {
+                _paddle2Direction = new Vector2(0,-1);
+            }
+            else if(kbState.IsKeyDown(Keys.S))
+            {
+                _paddle2Direction = new Vector2(0,1);
+            }
+            else
+            {
+                _paddle2Direction = Vector2.Zero;
+            }
+
+            _paddle2Position += _paddle2Direction * _paddle2Speed * dt;
+
+            if(_paddle2Position.Y <= PlayAreaBoundingBox.Top)
+            {
+                _paddle2Position.Y = PlayAreaBoundingBox.Top;   
+            }
+            else if(_paddle2Position.Y + _paddle2Dimensions.Y >= PlayAreaBoundingBox.Bottom)
+            {
+                _paddle2Position.Y = PlayAreaBoundingBox.Bottom - _paddle2Dimensions.Y;
+            }
+        #endregion
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        _spriteBatch.Begin();
+
+            _spriteBatch.Draw(_backgroundTexture, new Rectangle(0, 0, _WindowWidth, _WindowHeight), Color.White);
+
+            Rectangle ballRectangle = new Rectangle((int) _ballPosition.X, (int) _ballPosition.Y, _BallWidthAndHeight, _BallWidthAndHeight);
+            _spriteBatch.Draw(_ballTexture, ballRectangle, Color.White);
+
+            Rectangle paddleRectangle = new Rectangle((int)_paddlePosition.X, (int)_paddlePosition.Y, (int)_paddleDimensions.X, (int)_paddleDimensions.Y);
+            _spriteBatch.Draw(_paddleTexture, paddleRectangle, Color.White);
+
+            Rectangle paddle2Rectangle = new Rectangle((int)_paddle2Position.X, (int)_paddle2Position.Y, (int)_paddle2Dimensions.X, (int)_paddle2Dimensions.Y);
+            _spriteBatch.Draw(_paddleTexture, paddle2Rectangle, Color.White);
+        
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
